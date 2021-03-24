f6ec5aca5346219499b43ab14e2719e131ff9c1c
Julio Monterrosa <julio.monterrosa@udea.edu.co>
2021-03-23 21:40:52 -0500

    ensayo mapa y taller json

---
+++ fuentes/practica json/src/main/java/App.java
@@ -0,0 +1,40 @@
+import com.google.gson.Gson;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class App {
+    static Gson gson = new Gson();
+    private File fileJson;
+    private static final String jsonFilePath = "src/main/resources/User.json";
+    public static void main(String[] Args){
+        User userTransformation = new App().JsonToUserT();
+
+        System.out.println(String.format("Desde el objeto >>:\n%s\n",userTransformation));
+        String newJsonT = gson.toJson(userTransformation);
+        System.out.println(String.format("Transformacion hacia Json\n%s\n",newJsonT));
+    }
+
+    public User JsonToUserT(){
+        try{
+            fileJson = new File(jsonFilePath);
+            BufferedReader buffer = new BufferedReader(new FileReader(fileJson));
+            String json = "";
+            String line = "";
+            while ((line = buffer.readLine())!= null){
+                json = json + line;
+            }
+
+            buffer.close();
+
+            User user = gson.fromJson(json, User.class);
+            return user;
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
